e91823dce0c52b7ea5aef390ccf735d0989b35b9
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-29 05:52:00 +0000

    bed time yo

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,38 @@ public class PracticeProblem {
 
 	}
 
+	public static String evenOrOdd(int numIn) {
+		if (numIn == 0) {
+    	return "Zero";
+		} else if (numIn % 2 != 0){
+    	return "Odd";
+		}
+		else
+		return "Even";
+	}
+
+	public static String positiveOrNegative(int numIn) {
+		if (numIn == 0) {
+    	return "Zero";
+		} else if (numIn > 0){
+    	return "Positive";
+		}
+		else
+		return "Negative";
+	}
 	
+	public static String pluralize(String wordIn) {
+		wordIn = wordIn.toLowerCase();
+		if (wordIn.endsWith("ey")) {
+    	return "eys";
+		} else if (wordIn.endsWith("y")) {
+    	return "ies";
+		}
+		else if (wordIn.endsWith("ife")) {
+    	return "ives";
+		}
+		else 
+		return "s";
+	}
 
 }
